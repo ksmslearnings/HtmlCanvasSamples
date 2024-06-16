a3b6d45720bbe4aa91105ab1ced7b89d1d22d4e8
kunal <ksmslearnings@gmail.com>
2024-06-16 14:40:22 +0200

    more stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# HtmlCanvasSamples
-HtmlCanvasSamples
+# Open questions
+## html canvas pixel density and shapes dont work well when width height of canvas are changes.
+### like aquare become a rectangle kind of as width height is increased of canvas???
+### how to handle width and height of canvas properly - it all becomes pixeleted
+
+
--- canvas-script-arcs-lines.js
+++ canvas-script-arcs-lines.js
@@ -202,6 +202,131 @@ context.stroke();
  }
 
 
+//drawing shapes
+var shapeCanvas = document.getElementById('rect-canvas');
+shapescontext = shapeCanvas.getContext("2d");//also there is 3d context
+shapescontext.strokeStyle="red";
+shapescontext.fillStyle="blue";
+shapescontext.lineWidth=5;
+shapescontext.lineJoin="round";
+shapescontext.beginPath();
+shapescontext.rect(5,5,20,20);
+shapescontext.stroke();
+shapescontext.fill();//to fill color inside the shape.
 
+shapescontext.fillRect(5,30,20,20);//short form of fillRect but without fill and other line attributes
+shapescontext.lineWidth=2;
+shapescontext.lineWidth="square";
+shapescontext.strokeRect(5,65,30,30)
+
+//eraser
+//clear specific area - it uses rectangular format
+shapescontext.clearRect(0,0,10,15);
+//to clear all pass 0,0,canvas width and height
+
+//draw chess board
+let lightcolor = "#ddb180";
+let darkcolor = "#7c330c";
+shapescontext.beginPath();
+shapescontext.strokeStyle="black";
+shapescontext.lineWidth=0;
+shapescontext.lineJoin = "bevel";
+shapescontext.lineCap="miter";
+shapescontext.strokeRect(40,20,80,80);
+
+for(let x=1;x<=8;x++) 
+  {
+//row
+for(let y=1;y<=8;y++) {
+
+  if((x+y) % 2 == 0) {
+  shapescontext.fillStyle=darkcolor;
+  //shapescontext.fillRect(30 + i*10,10 + j*10,10,10);
+  }
+  else {
+  shapescontext.fillStyle=lightcolor;
+  //shapescontext.fillRect(30 + i*10,20,10,10);
+  }
+  shapescontext.fillRect(30 + x*10,10 + y*10,10,10);
+
+  }
+}
+
+//draw circle
+let radian1 = Math.PI / 180;
+shapescontext.beginPath();
+shapescontext.fillStyle="red";
+shapescontext.strokeStyle="red";
+shapescontext.arc(150,70,30,0 * radian1, 360 * radian1);
+shapescontext.stroke();
+shapescontext.fill();
+
+//draw a pac man shape
+shapescontext.beginPath();
+shapescontext.strokeStyle="orange";
+shapescontext.lineWidth=1;
+shapescontext.fillStyle="orange";
+shapescontext.moveTo(210,50);
+shapescontext.lineTo(240,70);
+shapescontext.arc(210,50,36,37*radian1, 323*radian, false);
+shapescontext.lineTo(210,50);
+shapescontext.stroke();
+shapescontext.fill();
+
+
+//second way to direct draw an arc and fill with color - we can draw multiple arcs and join together..
+shapescontext.beginPath();
+shapescontext.strokeStyle="orange";
+shapescontext.lineWidth=1;
+shapescontext.fillStyle="orange";
+shapescontext.arc(250,90,36,37*radian1, 130*radian, false);
+shapescontext.stroke();
+shapescontext.fill();
+
+//Making a polygon - using line by line is difficult since angle calculations is very hard.
+//Using circle approach and calculating coordinates using trignomatry becomes easier.
+//details in word document
+// we need centre coordinates of circle, radius and start angle.
+
+var polycanvas = document.getElementById('polygon-canvas');
+polycontext = polycanvas.getContext("2d");//also there is 3d context
+const centerX = 50;
+const centerY=50;
+const radius=30;
+const sides=5;
+const startAngle=200;
+const angle = (2 * Math.PI)/ sides;
+
+  // Start Drawing
+  polycontext.beginPath();
+  polycontext.strokeStyle = "red";
+  polycontext.lineWidth = 1;
+  polycontext.lineJoin = "round";
+
+  // Begining Point Coordinates
+  var beginX = centerX + radius * Math.cos(startAngle);
+  var beginY = centerY - radius * Math.sin(startAngle);
+
+  polycontext.moveTo(beginX, beginY);
+
+  // Draw Lines
+  for(var i = 1; i <= sides; i++){
+    // Current point'S coordinates
+    var currentAngle = startAngle + i * angle;
+    var currentPointX = centerX + radius * Math.cos(currentAngle);
+    var currentPointY = centerY - radius * Math.sin(currentAngle);
+
+    // Draw the line
+    polycontext.lineTo(currentPointX, currentPointY);
+  }
+
+  polycontext.closePath(); // to close paths manually
+  polycontext.stroke();
+
+
+
+  //adding shadows to objects
+  
 
 }
+
